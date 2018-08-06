0d8d978f93a13a7afa7539d9db523fd5330271ba
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2018-08-06 23:04:44 +0300

    initial commit

---
+++ src/material-table.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+
+export default class MaterialTable extends React.Component {
+  render() {
+    return (
+      <div> hello, i am material table </div>
+    );
+  }
+}
