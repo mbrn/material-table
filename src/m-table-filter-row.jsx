0e53482923702517b9f397a1885f86542cca476a
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2018-08-09 23:26:17 +0300

    filtering started. test and some improvements required

---
+++ src/m-table-filter-row.jsx
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import {
+  TableCell, TableRow, TextField, 
+  FormControl, InputLabel, Select,
+  Input, MenuProps, MenuItem, Checkbox, ListItemText
+} from '@material-ui/core'
+
+class MTableFilterRow extends React.Component {
+  
+  getComponentForColumn(columnDef) {
+    if(columnDef.field) {
+      if(columnDef.lookup) {
+        return (
+          <FormControl style={{width: '100%'}}>
+            <Select
+              multiple
+              value={columnDef.tableData.filterValue || []}
+              onChange={event => {
+                this.props.onFilterChanged(columnDef.tableData.id, event.target.value)
+              }}
+              input={<Input id="select-multiple-checkbox" />}
+              renderValue={selecteds => selecteds.map(selected => columnDef.lookup[selected]).join(', ')}
+              MenuProps={MenuProps}
+            >
+              {
+                Object.keys(columnDef.lookup).map(key => (
+                  <MenuItem key={key} value={key}>
+                    <Checkbox checked={columnDef.tableData.filterValue && columnDef.tableData.filterValue.indexOf(key.toString()) > -1} />
+                    <ListItemText primary={columnDef.lookup[key]} />
+                  </MenuItem>  
+                ))
+              }              
+            </Select>
+          </FormControl>
+        );
+      }
+      else {
+        return (
+          <TextField 
+            style={columnDef.isNumeric && {float:'right'}}
+            type={columnDef.isNumeric && 'number'}
+            value={columnDef.tableData.filterValue}
+            onChange={(event) => { this.props.onFilterChanged(columnDef.tableData.id, event.target.value) }}
+          />
+        );
+      }
+    }
+  }
+
+  render() {
+    return (
+      <TableRow  >
+        {this.props.columns.map(columnDef => (
+          <TableCell>
+            {this.getComponentForColumn(columnDef)}
+          </TableCell>
+        ))}
+      </TableRow>
+    );
+  }
+}
+
+export default MTableFilterRow
