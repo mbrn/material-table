7aaa58845a62f811e717f431ecd37a61b91170be
MEHMET BARAN (IT) <mehmetbaran@mehmetbaran.net>
2018-10-12 11:38:01 +0300

    index.d.ts added to package.json

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('./dist/material-table').default;
+// module.exports = require('./dist/material-table').default;
+export { default as MaterialTable } from './src/material-table';
