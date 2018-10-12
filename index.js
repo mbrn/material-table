660457b3375a8ce382d55f1cb2aa1a4784b5116f
MEHMET BARAN (IT) <mehmetbaran@mehmetbaran.net>
2018-10-12 12:15:48 +0300

    jsx to js

--- index.js
+++
@@ -1,6 +0,0 @@
-// module.exports = require('./dist/material-table').default;
-// export { default as MaterialTable } from './dist/material-table';
-
-// module.exports = { default : require('./dist/material-table').default }
-
-exports.default = require('./dist/material-table').default
