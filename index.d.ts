65dad6ea84021a66138bffe5413df4f2ae28bc34
MEHMET BARAN (IT) <mehmetbaran@mehmetbaran.net>
2018-10-12 10:55:42 +0300

    This closes #30, index.d.ts added

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+export interface MaterialTableProps {
+  actions?: Action[];
+  columns: Column[];
+  data: object;
+  title: string;
+  options?: Options;
+  localization?: Localization;
+}
+
+export interface Action {
+  icon: string;
+  isFreeAction?: boolean;
+  tooltip?: string;
+  onClick: (event: any, data: any) => void;
+}
+
+export interface Column {
+  cellStyle: any | ((data: any) => any);
+  hidden?: boolean;
+  field?: string;
+  lookup?: object;
+  render?: (data:any) => any;
+  title: string;
+  type?: 'boolean' | 'numeric' | 'date' | 'datetime' | 'time';
+}
+
+export interface Options {
+  columnsButton?: boolean;
+  filtering?: boolean;
+  paging?: boolean;
+  pageSize?: number;
+  pageSizeOptions?: number[];
+  search?: boolean;
+  selection?: boolean;
+  toolbar?: boolean;
+}
+
+export interface Localization {
+  actions?: string;
+  nRowsSelected?: string;
+}
+
+declare const MaterialTable: React.ComponentType<MaterialTableProps>;
+export default MaterialTable;
