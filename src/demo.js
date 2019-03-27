3bc73012cd5ae272e00fc1d38be1efd435291dd8
mbrn <mehmetbaran@mehmetbaran.net>
2019-03-27 14:15:08 +0300

    This closes #383, tableRef added

--- src/demo.js
+++ src/demo.js
@@ -69,48 +69,12 @@ class App extends Component {
               />
             </Grid>
 
-            <Grid item xs={12}>
-              <MaterialTable
-                tableRef={this.tableRef}
-                columns={[
-                  {
-                    title: 'Avatar',
-                    field: 'avatar',
-                    render: rowData => (
-                      <img
-                        style={{ height: 36, borderRadius: '50%' }}
-                        src={rowData.avatar}
-                      />
-                    ),
-                  },
-                  { title: 'Id', field: 'id' },
-                  { title: 'First Name', field: 'first_name' },
-                  { title: 'Last Name', field: 'last_name' },
-                ]}
-                data={query =>
-                  new Promise((resolve, reject) => {
-                    let url = 'https://reqres.in/api/users?';
-                    url += 'per_page=' + query.pageSize;
-                    url += '&page=' + (query.page + 1);
-                    fetch(url)
-                      .then(response => response.json())
-                      .then(result => {
-                        resolve({
-                          data: result.data,
-                          page: result.page - 1,
-                          totalCount: result.total,
-                        })
-                      });
-                  })
-                }
-                title="Remote Data Example"
-              />
-            </Grid>
+           
 
           </Grid>
           <button
             onClick={() => {
-              this.tableRef.current.onQueryChange();
+              // this.tableRef.current.onQueryChange();
             }}
           >
             ok
