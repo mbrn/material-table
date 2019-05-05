0c21cba689ea350c8c456a19d431d19825f0a35a
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2019-05-05 14:45:32 +0300

    webpack moved to demo folder

---
+++ demo/demo.js
@@ -0,0 +1,103 @@
+import { Grid, MuiThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MaterialTable from '../src';
+
+let direction = 'ltr';
+// direction = 'rtl';
+const theme = createMuiTheme({
+  direction: direction,
+  palette: {
+    type: 'light'
+  }
+});
+
+const bigData = [];
+for (let i = 0; i < 1; i++) {
+  const d = {
+    id: i + 1,
+    name: 'Name' + i,
+    surname: 'Surname' + Math.round(i / 10),
+    isMarried: i % 2 ? true : false,
+    birthDate: new Date(1987, 1, 1),
+    birthCity: 0,
+    sex: i % 2 ? 'Male' : 'Female',
+    type: 'adult',
+    insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+    time: new Date(1900, 1, 1, 14, 23, 35)
+  };
+  bigData.push(d);
+}
+
+class App extends Component {
+  tableRef = React.createRef();
+
+  colRenderCount = 0;
+
+  state = {
+    text: 'text',
+    selecteds: 0,
+    data: [
+      { id: 1, name: 'A1', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 0, sex: 'Male', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
+      { id: 2, name: 'A2', surname: 'B', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
+      { id: 3, name: 'A3', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
+      { id: 4, name: 'A4', surname: 'C', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 3 },
+      { id: 5, name: 'A5', surname: 'C', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
+      { id: 6, name: 'A6', surname: 'C', isMarried: true, birthDate: new Date(1989, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 5 },
+    ],
+    columns: [
+      { title: 'Adı', field: 'name', render: () => { 
+        this.colRenderCount++;
+        return "A"; 
+      }},
+      { title: 'Soyadı', field: 'surname' },
+      { title: 'Evli', field: 'isMarried', type: 'boolean' },
+      { title: 'Cinsiyet', field: 'sex', disableClick: true, editable: 'onAdd' },
+      { title: 'Tipi', field: 'type', removable: false, editable: 'never' },
+      { title: 'Doğum Yılı', field: 'birthDate', type: 'date' },
+      { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' } },
+      { title: 'Kayıt Tarihi', field: 'insertDateTime', type: 'datetime' },
+      { title: 'Zaman', field: 'time', type: 'time' }
+    ],
+    remoteColumns: [
+      { title: 'Avatar', field: 'avatar', render: rowData => <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} /> },
+      { title: 'Id', field: 'id' },
+      { title: 'First Name', field: 'first_name' },
+      { title: 'Last Name', field: 'last_name' },
+    ]
+  }
+
+  render() {
+    return (
+
+      <>
+        <MuiThemeProvider theme={theme}>
+          <input type="text" value={this.state.text} onChange={e => this.setState({ text: e.target.value, colRenderCount: this.colRenderCount })} />
+          {this.state.colRenderCount}          
+          <div style={{ maxWidth: '100%', direction }}>
+            <Grid container>
+              <Grid item xs={12}>
+                <MaterialTable
+                  ref={this.tableRef}
+                  columns={this.state.columns}
+                  data={this.state.data}
+                  // data={bigData}
+                  title="Demo Title"                  
+                />
+              </Grid>
+            </Grid>
+            {this.state.text}
+          </div>
+        </MuiThemeProvider>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
