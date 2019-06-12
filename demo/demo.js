824cb4cb2113fd675239125dd6a4221791a5d376
Stephane Royer <admroyer@hotmail.com>
2019-06-13 01:14:14 +0200

    grouping corrected for multi level

--- demo/demo.js
+++ demo/demo.js
@@ -104,53 +104,12 @@ class App extends Component {
                   title="Demo Title"
                   options={{
                     maxBodyHeight: '400px',
-                    grouping: true
+                    grouping: true,
                     selection: true,
                   }}
                 />
               </Grid>
             </Grid>
-            {this.state.text}
-            <button onClick={() => this.tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
-              Select
-            </button>
-            <MaterialTable
-              title="Remote Data Preview"
-              columns={[
-                {
-                  title: 'Avatar',
-                  field: 'avatar',
-                  render: rowData => (
-                    <img
-                      style={{ height: 36, borderRadius: '50%' }}
-                      src={rowData.avatar}
-                    />
-                  ),
-                },
-                { title: 'Id', field: 'id' },
-                { title: 'First Name', field: 'first_name' },
-                { title: 'Last Name', field: 'last_name' },
-              ]}
-              options={{
-                grouping: true,
-                filtering: true
-              }}
-              data={query => new Promise((resolve, reject) => {
-                let url = 'https://reqres.in/api/users?'
-                url += 'per_page=' + query.pageSize
-                url += '&page=' + (query.page + 1)
-                console.log(query);
-                fetch(url)
-                  .then(response => response.json())
-                  .then(result => {
-                    resolve({
-                      data: result.data,
-                      page: result.page - 1,
-                      totalCount: result.total,
-                    })
-                  })
-              })}
-            />
 
           </div>
         </MuiThemeProvider>
@@ -165,3 +124,48 @@ ReactDOM.render(
 );
 
 module.hot.accept();
+
+
+
+
+// {this.state.text}
+// <button onClick={() => this.tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
+//   Select
+// </button>
+// <MaterialTable
+//   title="Remote Data Preview"
+//   columns={[
+//     {
+//       title: 'Avatar',
+//       field: 'avatar',
+//       render: rowData => (
+//         <img
+//           style={{ height: 36, borderRadius: '50%' }}
+//           src={rowData.avatar}
+//         />
+//       ),
+//     },
+//     { title: 'Id', field: 'id' },
+//     { title: 'First Name', field: 'first_name' },
+//     { title: 'Last Name', field: 'last_name' },
+//   ]}
+//   options={{
+//     grouping: true,
+//     filtering: true
+//   }}
+//   data={query => new Promise((resolve, reject) => {
+//     let url = 'https://reqres.in/api/users?'
+//     url += 'per_page=' + query.pageSize
+//     url += '&page=' + (query.page + 1)
+//     console.log(query);
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(result => {
+//         resolve({
+//           data: result.data,
+//           page: result.page - 1,
+//           totalCount: result.total,
+//         })
+//       })
+//   })}
+// />
