647409fd1039b43437de9b96b305538de625c9f0
Hannes Junnila <hannes.junnila@gmail.com>
2019-06-26 09:59:20 +0300

    Fix usage of React import for typescript definitions
    Fixes #732

--- types/index.d.ts
+++ types/index.d.ts
@@ -152,23 +152,23 @@ export const MTableToolbar: () => React.ReactElement<any>;
 
 
 export interface Icons {
-  Add?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Check?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Clear?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Delete?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  DetailPanel?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Edit?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Export?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Filter?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  FirstPage?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  SortArrow?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  LastPage?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  NextPage?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  PreviousPage?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  ResetSearch?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  Search?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  ThirdStateCheck?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
-  ViewColumn?: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>>;
+  Add?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Check?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Clear?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Delete?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  DetailPanel?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Edit?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Export?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Filter?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  FirstPage?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  SortArrow?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  LastPage?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  NextPage?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  PreviousPage?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  ResetSearch?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  Search?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  ThirdStateCheck?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
+  ViewColumn?: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement>>;
 }
 
 export interface Options {
