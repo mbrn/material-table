ee98ef2d6698e899bd8fc8f5e0d0b5e4f8d99a86
Guilherme Monte <gmonte@inf.ufsm.br>
2019-06-26 00:07:19 -0300

    fix options.searchFieldProps position in m-table-toolbar props

--- src/components/m-table-toolbar.js
+++ src/components/m-table-toolbar.js
@@ -81,6 +81,7 @@ export class MTableToolbar extends React.Component {
           onChange={event => this.props.onSearchChanged(event.target.value)}
           placeholder={localization.searchPlaceholder}
           color="inherit"
+          {...this.props.searchFieldProps}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -105,7 +106,6 @@ export class MTableToolbar extends React.Component {
             style: this.props.searchFieldStyle,
             ...(this.props.searchFieldProps.InputProps || {})
           }}
-          {...this.props.searchFieldProps}
         />
       );
     } else {
