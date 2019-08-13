20af019fd76860b0dc1cca3c029753af91cb4481
Johan Skär Holm <johanskarholm@gmail.com>
2019-08-13 08:57:07 +0200

    Break out logic from demo.

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,9 @@
-import { Grid, MuiThemeProvider, Button } from '@material-ui/core';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { Grid, MuiThemeProvider } from '@material-ui/core';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MaterialTable from '../src';
-
-let direction = 'ltr';
-// direction = 'rtl';
-const theme = createMuiTheme({
-  direction: direction,
-  palette: {
-    type: 'light'
-  }
-});
+import { data, columns, remoteColumns } from './dummy';
+import theme from './theme';
 
 const bigData = [];
 for (let i = 0; i < 1; i++) {
@@ -25,7 +17,7 @@ for (let i = 0; i < 1; i++) {
     sex: i % 2 ? 'Male' : 'Female',
     type: 'adult',
     insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
-    time: new Date(1900, 1, 1, 14, 23, 35)
+    time: new Date(1900, 1, 1, 14, 23, 35),
   };
   bigData.push(d);
 }
@@ -35,41 +27,23 @@ class App extends Component {
 
   colRenderCount = 0;
 
-  state = {
-    text: 'text',
-    selecteds: 0,
-    data: [
-      { id: 1, name: 'A1', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 0, sex: 'Male', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 2, name: 'A2', surname: 'B', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 3, name: 'A3', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 4, name: 'A4', surname: 'Dede', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 3 },
-      { id: 5, name: 'A5', surname: 'C', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 6, name: 'A6', surname: 'C', isMarried: true, birthDate: new Date(1989, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 5 },
-    ],
-    columns: [
-      { title: 'Adı', field: 'name', filterPlaceholder: 'Adı filter' },
-      { title: 'Soyadı', field: 'surname', initialEditValue: 'test' },
-      { title: 'Evli', field: 'isMarried', type: 'boolean' },
-      { title: 'Cinsiyet', field: 'sex', disableClick: true, editable: 'onAdd' },
-      { title: 'Tipi', field: 'type', removable: false, editable: 'never' },
-      { title: 'Doğum Yılı', field: 'birthDate', type: 'date' },
-      { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' } },
-      { title: 'Kayıt Tarihi', field: 'insertDateTime', type: 'datetime' },
-      { title: 'Zaman', field: 'time', type: 'time' }
-    ],
-    remoteColumns: [
-      { title: 'Avatar', field: 'avatar', render: rowData => <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} /> },
-      { title: 'Id', field: 'id' },
-      { title: 'First Name', field: 'first_name', defaultFilter: 'De' },
-      { title: 'Last Name', field: 'last_name' },
-    ]
-  }
+  state = { remoteColumns, data, columns, text: 'text' };
+
+  onRowAdd = newData =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        {
+          /* const data = this.state.data; data.push(newData); this.setState({ data }, () => resolve()); */
+        }
+        resolve();
+      }, 1000);
+    });
 
   render() {
     return (
       <>
         <MuiThemeProvider theme={theme}>
-          <div style={{ maxWidth: '100%', direction }}>
+          <div style={{ maxWidth: '100%', direction: 'ltr' }}>
             <Grid container>
               <Grid item xs={12}>
                 <MaterialTable
@@ -82,53 +56,44 @@ class App extends Component {
                     columnsButton: true,
                     filtering: true
                   }}
-                  onSearchChange={(e) => console.log("search changed: " + e)}
-                  onColumnDragged={(oldPos, newPos) => console.log("Dropped column from " + oldPos + " to position " + newPos)}
+                  editable={{
+                    onRowAdd: this.onRowAdd,
+                  }}
                 />
               </Grid>
             </Grid>
             {this.state.text}
-            <button onClick={() => this.tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
+            <button
+              onClick={() => this.tableRef.current.onAllSelected(true)}
+              style={{ margin: 10 }}
+            >
               Select
             </button>
             <MaterialTable
               title="Remote Data Preview"
-              columns={[
-                {
-                  title: 'Avatar',
-                  field: 'avatar',
-                  render: rowData => (
-                    <img
-                      style={{ height: 36, borderRadius: '50%' }}
-                      src={rowData.avatar}
-                    />
-                  ),
-                },
-                { title: 'Id', field: 'id', filterPlaceholder: 'placeholder' },
-                { title: 'First Name', field: 'first_name' },
-                { title: 'Last Name', field: 'last_name' },
-              ]}
+              columns={remoteColumns}
               options={{
                 grouping: true,
                 filtering: true,
               }}
-              data={query => new Promise((resolve, reject) => {
-                let url = 'https://reqres.in/api/users?'
-                url += 'per_page=' + query.pageSize
-                url += '&page=' + (query.page + 1)
-                console.log(query);
-                fetch(url)
-                  .then(response => response.json())
-                  .then(result => {
-                    resolve({
-                      data: result.data,
-                      page: result.page - 1,
-                      totalCount: result.total,
-                    })
-                  })
-              })}
+              data={query =>
+                new Promise((resolve, reject) => {
+                  let url = 'https://reqres.in/api/users?';
+                  url += 'per_page=' + query.pageSize;
+                  url += '&page=' + (query.page + 1);
+                  /* eslint-disable-next-line no-undef */
+                  fetch(url)
+                    .then(response => response.json())
+                    .then(result => {
+                      resolve({
+                        data: result.data,
+                        page: result.page - 1,
+                        totalCount: result.total,
+                      });
+                    });
+                })
+              }
             />
-
           </div>
         </MuiThemeProvider>
       </>
@@ -136,9 +101,6 @@ class App extends Component {
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
 
 module.hot.accept();
