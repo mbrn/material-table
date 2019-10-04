2e546ab3fbfad40db71418ca345a767fac5ed2fd
Calvin <me@calvin.sh>
2019-10-04 00:16:12 -0700

    Change option from "header" to "toolbar"

--- types/index.d.ts
+++ types/index.d.ts
@@ -69,7 +69,7 @@ export interface Action<RowData extends object> {
   disabled?: boolean;
   icon: string | (() => React.ReactElement<any>);
   isFreeAction?: boolean;
-  position?: 'auto' | 'header' | 'headerOnSelect' | 'row';
+  position?: 'auto' | 'toolbar' | 'toolbarOnSelect' | 'row';
   tooltip?: string;
   onClick: (event: any, data: RowData | RowData[]) => void;
   iconProps?: IconProps;
