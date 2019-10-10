a0391abe3c614ffd3b7e5ce1750a59cd9c8a7beb
Thanh Trung NGUYEN <flyingangel1987@gmail.com>
2019-10-10 21:52:18 +0200

    Allow integer 0 as default value

--- src/components/m-table-edit-row.js
+++ src/components/m-table-edit-row.js
@@ -19,7 +19,7 @@ export default class MTableEditRow extends React.Component {
   }
 
   createRowData(){
-    return this.props.columns.filter(column=>column.initialEditValue && column.field).reduce((prev,column)=>{
+    return this.props.columns.filter(column => (column.initialEditValue || column.initialEditValue === 0) && column.field).reduce((prev,column)=>{
       prev[column.field]=column.initialEditValue;
       return prev;
     },{});
