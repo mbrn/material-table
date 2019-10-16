d8cafad9dd488a4d898d925369e45f92890e9448
Dustin Robison <Dustinrobi@gmail.com>
2019-10-16 16:33:10 -0700

    Updated demo.js to use hooks

--- demo/demo.js
+++ demo/demo.js
@@ -31,42 +31,40 @@ for (let i = 0; i < 1; i++) {
   bigData.push(d);
 }
 
-class App extends Component {
-  tableRef = React.createRef();
+const App = () => {
+  const tableRef = React.createRef();
+  const [state, setState] = React.useState({
+     text: 'text',
+     selecteds: 0,
+     data: [
+       { id: 1, name: 'A1', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 0, sex: 'Male', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
+       { id: 2, name: 'A2', surname: 'B', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
+       { id: 3, name: 'A3', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
+       { id: 4, name: 'A4', surname: 'Dede', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 3 },
+       { id: 5, name: 'A5', surname: 'C', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
+       { id: 6, name: 'A6', surname: 'C', isMarried: true, birthDate: new Date(1989, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 5 },
+     ],
+     columns: [
+       { title: 'Adı', field: 'name', filterPlaceholder: 'Adı filter' },
+       { title: 'Soyadı', field: 'surname', initialEditValue: 'test' },
+       { title: 'Evli', field: 'isMarried', type: 'boolean' },
+       { title: 'Cinsiyet', field: 'sex', disableClick: true, editable: 'onAdd' },
+       { title: 'Tipi', field: 'type', removable: false, editable: 'never' },
+       { title: 'Doğum Yılı', field: 'birthDate', type: 'date' },
+       { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' } },
+       { title: 'Kayıt Tarihi', field: 'insertDateTime', type: 'datetime' },
+       { title: 'Zaman', field: 'time', type: 'time' }
+     ],
+     remoteColumns: [
+       { title: 'Avatar', field: 'avatar', render: rowData => <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} /> },
+       { title: 'Id', field: 'id' },
+       { title: 'First Name', field: 'first_name', defaultFilter: 'De' },
+       { title: 'Last Name', field: 'last_name' },
+     ]
+   });
 
-  colRenderCount = 0;
+   const colRenderCount = 0;
 
-  state = {
-    text: 'text',
-    selecteds: 0,
-    data: [
-      { id: 1, name: 'A1', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 0, sex: 'Male', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 2, name: 'A2', surname: 'B', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 3, name: 'A3', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 4, name: 'A4', surname: 'Dede', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 3 },
-      { id: 5, name: 'A5', surname: 'C', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 6, name: 'A6', surname: 'C', isMarried: true, birthDate: new Date(1989, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 5 },
-    ],
-    columns: [
-      { title: 'Adı', field: 'name', filterPlaceholder: 'Adı filter' },
-      { title: 'Soyadı', field: 'surname', initialEditValue: 'test' },
-      { title: 'Evli', field: 'isMarried', type: 'boolean' },
-      { title: 'Cinsiyet', field: 'sex', disableClick: true, editable: 'onAdd' },
-      { title: 'Tipi', field: 'type', removable: false, editable: 'never' },
-      { title: 'Doğum Yılı', field: 'birthDate', type: 'date' },
-      { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' } },
-      { title: 'Kayıt Tarihi', field: 'insertDateTime', type: 'datetime' },
-      { title: 'Zaman', field: 'time', type: 'time' }
-    ],
-    remoteColumns: [
-      { title: 'Avatar', field: 'avatar', render: rowData => <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} /> },
-      { title: 'Id', field: 'id' },
-      { title: 'First Name', field: 'first_name', defaultFilter: 'De' },
-      { title: 'Last Name', field: 'last_name' },
-    ]
-  }
-
-  render() {
     return (
       <>
         <MuiThemeProvider theme={theme}>
@@ -74,9 +72,9 @@ class App extends Component {
             <Grid container>
               <Grid item xs={12}>
                 <MaterialTable
-                  tableRef={this.tableRef}
-                  columns={this.state.columns}
-                  data={this.state.data}
+                  tableRef={tableRef}
+                  columns={state.columns}
+                  data={state.data}
                   title="Demo Title"
                   options={{
                     selection: true,
@@ -90,8 +88,8 @@ class App extends Component {
                 />
               </Grid>
             </Grid>
-            {this.state.text}
-            <button onClick={() => this.tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
+            {state.text}
+            <button onClick={() => tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
               Select
             </button>
             <MaterialTable
@@ -138,7 +136,6 @@ class App extends Component {
         </MuiThemeProvider>
       </>
     );
-  }
 }
 
 ReactDOM.render(
