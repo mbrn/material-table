5a121a6e705a9472c3da881d0ca54ed4e8c0f7b7
Domino987 <domino051@gmail.com>
2019-10-31 12:02:43 +0100

    Update m-table-body.js

--- src/components/m-table-body.js
+++ src/components/m-table-body.js
@@ -46,8 +46,8 @@ class MTableBody extends React.Component {
             columns={this.props.columns.filter(columnDef => { return !columnDef.hidden })}
             components={this.props.components}
             data={data}
-            icons={this.props.icons}
-            localization={{ ...MTableBody.defaultProps.localization.editRow, ...this.props.localization.editRow, dateTimePickerLocalization: this.props.localization.dateTimePickerLocalization }}
+            icons={this.props.icons}            
+            localization={{ ...MTableBody.defaultProps.localization.editRow, ...this.props.localization.editRow }}
             key={index}
             mode={data.tableData.editing}
             options={this.props.options}
