14fbe5b275d7f02de2ec016505d98a47aa8092da
Scott Adams <scottadams80@gmail.com>
2019-11-10 22:02:49 +0100

    Remove color="inherit" from TextField. Fixes #1293.

--- src/components/m-table-toolbar.js
+++ src/components/m-table-toolbar.js
@@ -65,7 +65,6 @@ export class MTableToolbar extends React.Component {
           value={this.props.searchText}
           onChange={event => this.props.onSearchChanged(event.target.value)}
           placeholder={localization.searchPlaceholder}
-          color="inherit"
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
