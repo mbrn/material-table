f411529374085089260b957e224cac3fefe31bf6
Daniel Kinsbursky <dkinsbursky@kinsbursky.com>
2019-11-21 09:15:18 -0800

    Set row equal to a new object, with new ref, to prevent Material-Table from mutating variable passed through the data prop;

--- src/utils/data-manager.js
+++ src/utils/data-manager.js
@@ -47,7 +47,7 @@ export default class DataManager {
     this.selectedCount = 0;
 
     this.data = data.map((row, index) => {
-      row.tableData = { ...row.tableData, id: index };
+      row = {...row, tableData: { ...row.tableData, id: index }};
       if (row.tableData.checked) {
         this.selectedCount++;
       }
