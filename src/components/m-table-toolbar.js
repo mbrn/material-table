17ba867870d39227023d504c99bfa6159cdfa209
Raj Thapa <ga82507@delta.org>
2020-02-27 16:07:51 -0500

    Updated actions to show toolbar actions even when selection is made

--- src/components/m-table-toolbar.js
+++ src/components/m-table-toolbar.js
@@ -173,7 +173,7 @@ export class MTableToolbar extends React.Component {
   renderSelectedActions() {
     return (
       <React.Fragment>
-        <this.props.components.Actions actions={this.props.actions.filter(a => a.position === "toolbarOnSelect")} data={this.props.selectedRows} components={this.props.components} />
+        <this.props.components.Actions actions={this.props.actions.filter(a => { return (a.position === "toolbarOnSelect" || a.position === "toolbar"); })} data={this.props.selectedRows} components={this.props.components} />
       </React.Fragment>
     );
   }
