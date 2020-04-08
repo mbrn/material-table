088c681e268a3c622979589afa68e7b1c0500629
Ross Jackson <RossJackson@Deltek.com>
2020-04-08 14:34:24 -0400

    Remove hidden columns in computing the colspan of an empty tablecell so it won't have extra spaces on the right of the table.

--- src/components/m-table-body.js
+++ src/components/m-table-body.js
@@ -26,7 +26,7 @@ class MTableBody extends React.Component {
       }
       return (
         <TableRow style={{ height: rowHeight * (this.props.options.paging && this.props.options.emptyRowsWhenPaging ? this.props.pageSize : 1) }} key={'empty-' + 0} >
-          <TableCell style={{ paddingTop: 0, paddingBottom: 0, textAlign: 'center' }} colSpan={this.props.columns.length + addColumn} key="empty-">
+          <TableCell style={{ paddingTop: 0, paddingBottom: 0, textAlign: 'center' }} colSpan={this.props.columns.reduce((currentVal, columnDef) => columnDef.hidden ? currentVal : currentVal + 1, 0) + addColumn} key="empty-">
             {localization.emptyDataSourceMessage}
           </TableCell>
         </TableRow>
