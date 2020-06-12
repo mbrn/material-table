516a52d11db09b4e13a03ca521b44229f86581c1
Rashad Hourie <rashadhourie@gmail.com>
2020-06-13 00:31:31 +0300

    updated setByString
    updated setByString when column type is numeric value will be returned as number

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,18 +17,18 @@ export const byString = (o, s) => {
   return o;
 };
 
-export const setByString = (obj, path, value) => {
-  var schema = obj;  // a moving reference to internal objects within obj
+export const setByString = (obj, colDef, value) => {
+    var schema = obj;  // a moving reference to internal objects within obj
+    var path = colDef.field;
+    path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    path = path.replace(/^\./, '');           // strip a leading dot
+    var pList = path.split('.');
+    var len = pList.length;
+    for (var i = 0; i < len - 1; i++) {
+        var elem = pList[i];
+        if (!schema[elem]) schema[elem] = {};
+        schema = schema[elem];
+    }
 
-  path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-  path = path.replace(/^\./, '');           // strip a leading dot
-  var pList = path.split('.');
-  var len = pList.length;
-  for(var i = 0; i < len-1; i++) {
-      var elem = pList[i];
-      if( !schema[elem] ) schema[elem] = {};
-      schema = schema[elem];
-  }
-
-  schema[pList[len-1]] = value;
+    schema[pList[len - 1]] = colDef.type && colDef.type == "numeric" ? Number(value) : value;
 };
