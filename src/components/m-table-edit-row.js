720440a037de3d627ed3c5ee107e42d5de89f48d
Dominik Engel <domino051@gmail.com>
2020-08-12 21:35:13 +0200

    Fixes onBulkEditRowChanged on edit

--- src/components/m-table-edit-row.js
+++ src/components/m-table-edit-row.js
@@ -142,14 +142,18 @@ export default class MTableEditRow extends React.Component {
                   const data = { ...this.state.data };
                   setByString(data, columnDef.field, value);
                   // data[columnDef.field] = value;
-                  this.setState({ data }, () =>
-                    this.props.onBulkEditRowChanged(this.props.data, data)
-                  );
+                  this.setState({ data }, () => {
+                    if (this.props.onBulkEditRowChanged) {
+                      this.props.onBulkEditRowChanged(this.props.data, data);
+                    }
+                  });
                 }}
                 onRowDataChange={(data) => {
-                  this.setState({ data }, () =>
-                    this.props.onBulkEditRowChanged(this.props.data, data)
-                  );
+                  this.setState({ data }, () => {
+                    if (this.props.onBulkEditRowChanged) {
+                      this.props.onBulkEditRowChanged(this.props.data, data);
+                    }
+                  });
                 }}
               />
             </TableCell>
