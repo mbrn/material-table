1a4a048b2a39bf4f9da5fb02852cf6fe76310f73
AlexOxorn <alex.oxorn@gmail.com>
2020-08-20 13:01:56 -0400

    added type information

--- types/index.d.ts
+++ types/index.d.ts
@@ -302,7 +302,7 @@ export interface Options<RowData extends object> {
   draggable?: boolean;
   emptyRowsWhenPaging?: boolean;
   exportAllData?: boolean;
-  exportButton?: boolean;
+  exportButton?: boolean | { csv?: boolean; pdf?: boolean };
   exportDelimiter?: string;
   exportFileName?:
     | string
