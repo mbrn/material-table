c9c7eb20d134d79dcab98f14342a8825bd50f1e8
Dominik Engel <domino051@gmail.com>
2020-10-02 01:21:39 +0200

    WhileLoopRemoval

--- src/utils/common-values.js
+++ src/utils/common-values.js
@@ -10,23 +10,5 @@ export const selectionMaxWidth = (props, maxTreeLevel) =>
   baseIconSize(props) + 9 * maxTreeLevel;
 
 export const reducePercentsInCalc = (calc, fullValue) => {
-  let index = calc.indexOf("%");
-  let count = 0;
-  while (index !== -1 && count++ < 30) {
-    let leftIndex = index - 1;
-    while (leftIndex >= 0 && "0123456789.".indexOf(calc[leftIndex]) !== -1) {
-      leftIndex--;
-    }
-    leftIndex++;
-
-    const value = Number.parseFloat(calc.substring(leftIndex, index));
-    calc =
-      calc.substring(0, leftIndex) +
-      (value * fullValue) / 100 +
-      "px" +
-      calc.substring(index + 1);
-    index = calc.indexOf("%");
-  }
-
-  return calc;
+  return calc.replace(/\d*%/, `${fullValue}px`);
 };
