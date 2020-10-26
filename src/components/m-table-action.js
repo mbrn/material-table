fdf806c40534e4c64a028065f3f0a88fd8e95390
Md Mahfuzul Haque Muftee <mhmuftee@gmail.com>
2020-10-26 20:58:54 +0100

    feature: change tooltip after click for certain action

--- src/components/m-table-action.js
+++ src/components/m-table-action.js
@@ -4,9 +4,17 @@ import PropTypes from "prop-types";
 import Icon from "@material-ui/core/Icon";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 /* eslint-enable no-unused-vars */
 
 class MTableAction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tooltip: this.props.action.tooltip,
+    };
+  }
+
   render() {
     let action = this.props.action;
 
@@ -30,9 +38,21 @@ class MTableAction extends React.Component {
 
     const disabled = action.disabled || this.props.disabled;
 
+    const tooltip = this.state.tooltip ? this.state.tooltip : action.tooltip;
+
+    const handleTooltipClose = () => {
+      setTooltipMessage(action.tooltip);
+    };
+
+    const setTooltipMessage = (tooltipMessage) => {
+      this.setState({ tooltip: tooltipMessage });
+    };
+
     const handleOnClick = (event) => {
       if (action.onClick) {
         action.onClick(event, this.props.data);
+        if (action.tooltipAfterClick)
+          setTooltipMessage(action.tooltipAfterClick);
         event.stopPropagation();
       }
     };
@@ -57,15 +77,19 @@ class MTableAction extends React.Component {
       </IconButton>
     );
 
-    if (action.tooltip) {
+    if (tooltip) {
       // fix for issue #1049
       // https://github.com/mbrn/material-table/issues/1049
       return disabled ? (
-        <Tooltip title={action.tooltip}>
+        <Tooltip title={tooltip}>
           <span>{button}</span>
         </Tooltip>
       ) : (
-        <Tooltip title={action.tooltip}>{button}</Tooltip>
+        <ClickAwayListener onClickAway={handleTooltipClose}>
+          <Tooltip onClose={handleTooltipClose} title={tooltip}>
+            {button}
+          </Tooltip>
+        </ClickAwayListener>
       );
     } else {
       return button;
