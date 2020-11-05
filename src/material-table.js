fe31bef9aae5985d33efa45728bc7d8399ef9170
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-11-05 18:45:55 +0100

    Call changeRowEditing only on init to prevent row editing state loss

--- src/material-table.js
+++ src/material-table.js
@@ -84,7 +84,7 @@ export default class MaterialTable extends React.Component {
 
     this.dataManager.setColumns(props.columns);
     this.dataManager.setDefaultExpanded(props.options.defaultExpanded);
-    this.dataManager.changeRowEditing();
+    isInit && this.dataManager.changeRowEditing();
 
     if (this.isRemoteData(props)) {
       this.dataManager.changeApplySearch(false);
