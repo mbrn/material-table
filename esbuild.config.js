9b44f354d3c760d6a9c8f851ffd86f1371648409
Matt Oestreich <matthewpoestreich@gmail.com>
2021-01-08 23:04:44 -0600

    add esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,49 @@
+const { build } = require('esbuild');
+const rimraf = require('rimraf');
+const path = require('path');
+
+const pkg = require('./package.json');
+
+const externalDependencies = Object.keys(pkg.peerDependencies).map(
+  (key) => key
+);
+
+console.log({ externalDependencies });
+
+rimraf(path.resolve(__dirname, './dist'), (error) => {
+  if (error) {
+    console.log("error : error removing old build files at './dist' " + error);
+  }
+
+  const options = {
+    entryPoints: ['src/index.js'],
+    minify: true,
+    bundle: true,
+    format: 'esm',
+    outfile: 'dist/material-table.js',
+    loader: {
+      '.js': 'jsx',
+    },
+    // These are third party libraries that we DO NOT want to
+    // include in our bundle. It is assumed the consumer will
+    // already have these packages. Including them in our bundle
+    // only bloats the size for no reason.
+    external: [
+      ...externalDependencies,
+      'react-beautiful-dnd',
+      /material-ui/,
+      'jspdf',
+      'jspdf-autotable',
+      '@material-ui',
+      'react-is',
+      'classnames',
+      'object-assign',
+      'prop-types',
+    ],
+  };
+
+  build(options).catch((err) => {
+    process.stderr.write(err.stderr);
+    process.exit(1);
+  });
+});
