41a7d161a992f309c83c384451e26367ee728390
mm0hammadi <mm34549@gmail.com>
2023-02-10 23:42:00 +0330

    feat: add fa localization

---
+++ src/localization/fa.js
@@ -0,0 +1,41 @@
+export const BMaterialTableLocalization = {
+  error: "خطا در دریافت اطلاعات",
+  header: {
+    actions: "عملیات",
+  },
+  body: {
+    filterRow: {
+      filterTooltip: "جستجوی ستون",
+    },
+    editRow: {
+      deleteText: "آیا از حذف این ردیف اطمینان دارید؟",
+      cancelTooltip: "انصراف",
+      saveTooltip: "ذخیره",
+    },
+    addTooltip: "افزودن",
+    editTooltip: "ویرایش",
+    deleteTooltip: "حذف",
+    emptyDataSourceMessage: "هیچ موردی جهت نمایش وجود ندارد",
+    bulkEditTooltip: "ویرایش همه",
+    bulkEditApprove: "ذخیره تغییرات",
+    bulkEditCancel: "انصراف",
+  },
+  toolbar: {
+    searchTooltip: "جستجو",
+    searchPlaceholder: "جستجو",
+    exportName: "فایل اکسل",
+    exportTitle: "دانلود اکسل",
+    showColumnsTitle: "نمایش فیلدها",
+    addRemoveColumns: "لیست فیلدهای قابل نمایش",
+    exportCSVName: "خروجی اکسل",
+    exportPDFName: "",
+  },
+  pagination: {
+    firstTooltip: "اولین صفحه",
+    previousTooltip: "صفحه قبلی",
+    nextTooltip: "صفحه بعدی",
+    lastTooltip: "آخرین صفحه",
+    labelDisplayedRows: "{from}-{to} از {count}",
+    labelRowsSelect: "سطر",
+  },
+};
