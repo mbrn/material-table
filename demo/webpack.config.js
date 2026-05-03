de39402f9eb82d75285546dc95c2d55664a17b14
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2026-05-03 14:27:14 +0300

    version 2.2.0

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -6,8 +6,21 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
+        exclude: /node_modules\/(?!(@mui)\/).*/, // @mui hariç node_modules exclude
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-proposal-nullish-coalescing-operator",
+              "@babel/plugin-proposal-logical-assignment-operators",
+            ],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
     ],
   },
